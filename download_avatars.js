33ff1b0a87fb1cbea1eb65e9e3e48576838629c9
MachoDhogo <chriswvliew@gmail.com>
2018-04-17 03:14:01 +0000

    Step 7 Print avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,11 @@
 var request = require('request');
-var secrets = require('secrets');
+var secrets = require('./secrets');
+
+// Your next and final step in this exercise should be to change your getRepoContributors function to
+//parse the JSON string into an object and pass this object (an array of contributor objects) to the cb function.
+
+// You will also need to modify the callback function to iterate over the results and (for now)
+// console.log the value for each avatar_url in the collection:
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -7,17 +13,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
-
+      'User-Agent': 'request',
+      'Authorization': "token " + secrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var parse = JSON.parse(body); // JSON parses the body of the results
+    cb(err, parse);
   });
 }
 
+// HOW TO CHECK IT AUTHENTICATED?
+// DID I DO AUTHENTICATION CORRECT?
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
+  result.forEach(function(element){
+    console.log(element.avatar_url)
+})
 });
+
+
+
