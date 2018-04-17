0a39f8af6db470e904b6e2bd667f1bfbb8c68ddc
MachoDhogo <chriswvliew@gmail.com>
2018-04-17 14:02:20 +0000

    Connect functions

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,11 @@
 var request = require('request');
 var secrets = require('./secrets');
 
-// Your next and final step in this exercise should be to change your getRepoContributors function to
-//parse the JSON string into an object and pass this object (an array of contributor objects) to the cb function.
-
-// You will also need to modify the callback function to iterate over the results and (for now)
-// console.log the value for each avatar_url in the collection:
-
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
+//This declarative function authenticates user on Github and JSON parses the data given
+//cb is the callback function that is passed back to the invoking function
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -19,44 +16,34 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    var parse = JSON.parse(body); // JSON parses the body of the results
+    var parse = JSON.parse(body);
     cb(err, parse);
   });
 }
 
-// HOW TO CHECK IT AUTHENTICATED?
-// DID I DO AUTHENTICATION CORRECT?
-
+//this function loops through the Github avatars and outputs each URL and filepath to the function downloadImagebyURL
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  // console.log("Result:", result);
-  result.forEach(function(element){
-    console.log(element.avatar_url)
+  result.forEach(function(element) {
+    var avatarURL = element.avatar_url;
+    var createdFilePath = "./avatars" + element.login + ".jpg";
+    downloadImageByURL(avatarURL, createdFilePath);
   })
 });
-
 
 var request = require('request');
 var fs = require('fs');
 
+//this function receives the URL and filepath from above which is the incoming readable data stream, transforms to write stream, then creates a new filepath into local directory and streams data into it
 function downloadImageByURL(url, filePath) {
-  request.get(url + filePath) //request.get equivalent to request()
+  request.get(url)
     .on('error', function(err) {
-      throw err;  //request.on('error', callback handles any error)
+      throw err;
     })
-
-    .on('response', function (response) {
-      console.log(response.statusCode + " " + response.statusMessage + " " + response.headers['content-type']);
-    })
-
-    .on('end', function(){
-      console.log('Downloading complete.');
-    })
-
-.pipe(fs.createWriteStream('./avatar.jpg')); //handles incoming readable data stream, transforms to write stream, then creates a future.jpg into local directory and streams data into it
+    .pipe(fs.createWriteStream(filePath));
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
+
 
 
 
